9c22b39296e5aa4c8cf1f554f05db2d016c44554
MariaLuizaSant <luiza2328@gmail.com>
2019-07-12 15:46:26 -0300

    aula T203

---
+++ script.js
@@ -0,0 +1,22 @@
+// function somar(a=20,b=9){
+//     return Number(a + b)
+// }
+
+// function fazerEscadinha(material, degraus){
+//     let escada = material;
+//     for(let i=0; i< degraus; i++){
+//         console.log(escada);
+//         escada += material;
+//     }
+// }
+
+// function parOuImpar(a){
+//     if(a % 2 ==0){
+//         return 'É par!'
+//     }
+//     else 'É impar'
+// }
+// const aleatorio = function (min, max){
+//     return Math.floor(Math.random()*(min - max) ) + min;
+// }
+
